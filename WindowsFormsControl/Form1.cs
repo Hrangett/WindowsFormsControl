6c058402d9c74687cf98a13e84c3eeaf4d9b0d59
Hrangett <100149118+Hrangett@users.noreply.github.com>
2022-05-23 15:12:57 +0900

    초기 커밋.

---
+++ WindowsFormsControl/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //매개변수 1 : 메세지 박스의 본문 메세지
+            //매개변수 2 : 메세지 박스의 타이틀
+            //매개변수 3 : 획인, 취소 버튼
+            //매개변수 4 : 아이콘
+
+            //MessageBox.Show("기본적인 메세지 박스");
+            //MessageBox.Show("타이틀을 표시하느 ㄴ메세지 박스", "타이틀");
+            
+            //DialogResult result = 
+            //    MessageBox.Show("버튼을 표시하느 ㄴ메세지 밗,ㅡ", "button", MessageBoxButtons.OKCancel);
+            //DialogResult result =
+            //   MessageBox.Show("버튼을 표시하느 ㄴ메세지 밗,ㅡ", "button", MessageBoxButtons.YesNoCancel, MessageBoxDefaultButton.Button3);
+
+            //if (result == DialogResult.OK)
+            //{
+            //    MessageBox.Show("예스버틍");
+            //}
+        }
+
+        private void maskedTextBox5_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsControl/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsControl
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string chkResult = "";
+            CheckBox[] chkBox = { checkBox1, checkBox2, checkBox3 };
+            
+            foreach(var item in chkBox)
+            {
+                chkResult
+                    += string.Format("{0} : {1}\n", item.Text, item.Checked);
+
+            }
+
+            MessageBox.Show(chkResult, "chkResult");
+
+        }
+    }
+}
